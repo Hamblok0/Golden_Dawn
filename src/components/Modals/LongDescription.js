91cffaba6275f8545d2965efc2f1b4a95331a497
Hambloko <hambloko@gmail.com>
2019-11-02 21:53:43 -0500

    card description modal functionality implemented, proper styling still needed

--- src/components/Modals/LongDescription.js
+++ src/components/Modals/LongDescription.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const LongDescription = props => {
     return (
-        <div className="longDescBody">
-            <p1>Test</p1>
+        <div className="longDescModal">
+            <button onClick={() => props.toggleLong(null)}>Exit</button>
+            <div className="longDescTitle">
+                <h1>{props.card.name}</h1>
+            </div>
+            <div className="longDescBody">
+                <p>Short Description: {props.card.shortDescription}</p>
+                <p>Detail: {props.card.longDescription}</p>
+            </div>
         </div>
     )
 };
