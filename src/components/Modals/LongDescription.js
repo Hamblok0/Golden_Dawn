1a4f87d7f5fea699faa1a59e14512438e8bc4fc9
Hambloko <hambloko@gmail.com>
2019-11-05 20:55:09 -0600

    made entire HTML fit onto screen without need for scrolling but this is a temporary solution to figure out why the long description modal is so buggy, eventually want to implement this style of rendering on all resolutions as temporary fix only looks decent in resolutions 1080p or smaller

--- src/components/Modals/LongDescription.js
+++ src/components/Modals/LongDescription.js
@@ -2,17 +2,19 @@ import React from "react";
 
 const LongDescription = props => {
     return (
-        <div className="longDescModal">
-            <button onClick={() => props.toggleLong(null)}>Exit</button>
-            <div className="longDescTitle">
-                <h1>{props.card.name}</h1>
-            </div>
-            <div>
-                <img src={props.card.url}/>
-            </div>
-            <div className="longDescBody">
-                <p>Short Description: {props.card.shortDescription}</p>
-                <p>Detail: {props.card.longDescription}</p>
+        <div className="longDescWrapper">
+            <div className="longDescModal">
+                <button onClick={() => props.toggleLong(null)}>Exit</button>
+                <div className="longDescTitle">
+                    <h1>{props.card.name}</h1>
+                </div>
+                <div>
+                    <img src={props.card.url}/>
+                </div>
+                <div className="longDescBody">
+                    <p>Short Description: {props.card.shortDescription}</p>
+                    <p>Detail: {props.card.longDescription}</p>
+                </div>
             </div>
         </div>
     )
