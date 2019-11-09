a0119230c0d393b198c2b08b8c2c246bcd3d7e94
Hambloko <hambloko@gmail.com>
2019-11-09 14:09:22 -0600

    login/signup modals in place and styled, no functionality yet

--- src/components/Modals/LogIn.js
+++ src/components/Modals/LogIn.js
@@ -7,38 +7,34 @@ const LogIn = props => {
         <div className="loginWrapper">
             {type === "login" &&
                 <div className="loginBody">
-                    <h1>Sign In</h1>
-                    <form>
-                        <label>
-                            Username:
+                    <div className="loginContent">
+                        <div className="loginTitle">
+                            <h1>Sign In</h1>
+                        </div>
+                        <form>
+                            <h2>Username:</h2>
                             <input type="text" name="username"/>
-                        </label>
-                        <label>
-                            Password:
+                            <h2>Password:</h2>
                             <input type="text" name="password"/>
-                        </label>
-                    </form>
-                    <div className="loginChange">
-                        <p onClick={() => setType("register")}>Don't have an account?</p>
+                        </form>
+                        <div className="loginChange">
+                            <p onClick={() => setType("register")}>Don't have an account?</p>
+                        </div>
                     </div>
                 </div>
             }
             {type === "register" &&
                 <div className="loginBody">
-                    <h1>Register</h1>
+                    <div className="loginTitle">
+                        <h1>Register</h1>
+                    </div>
                     <form>
-                        <label>
-                            Username:
-                            <input type="text" name="username"/>
-                        </label>
-                        <label>
-                            Password:
-                            <input type="text" name="password"/>
-                        </label>
-                        <label>
-                            Email:
-                            <input type="text" name="email"/>
-                        </label>
+                        <h2>Username:</h2>
+                        <input type="text" name="username"/>
+                        <h2>Password:</h2>
+                        <input type="text" name="password"/>
+                        <h2>Email:</h2>
+                        <input type="text" name="email"/>
                     </form>
                     <div className="loginChange">
                         <p onClick={() => setType("login")}>Already have an account?</p>
