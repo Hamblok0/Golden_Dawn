cdbf6837f340df0fd7d134ad68e056feab5ee057
Hambloko <hambloko@gmail.com>
2019-11-10 15:04:38 -0600

    read history page init

---
+++ src/components/ReadHistory.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+const ReadHistory = () => {
+    const endpoint = "https://ds7jrtsekfc2s.cloudfront.net/t-1.png" 
+    return (
+        <div className="historyWrapper">
+            <div className="historyTitle">
+                <h1>Saved Readings</h1>
+            </div>
+            <div className="historyLinks">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ReadHistory;
