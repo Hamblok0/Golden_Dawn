9ad28a1eacc8ec00149a85a82929f109e13fe5c5
Hambloko <hambloko@gmail.com>
2019-11-10 14:53:14 -0600

    user drop down modal template in place

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
 import LogIn from "./Modals/LogIn";
+import UserDrop from "./Modals/UserDrop";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
-  const [loggedIn, setLogIn] = useState(false);
+  const [loggedIn, setLogIn] = useState(true);
   const [logInToggle, toggleLogIn] = useState(false);
+  const [userDrop, toggleUserDrop] = useState(false);
   return (
     <header>
       <div className="title">
@@ -13,9 +16,11 @@ const Header = () => {
       <div className="headerNav">
         <h1>Get A Reading</h1>
         <h1>Card Dictionary</h1>
-        {loggedIn ? (<FontAwesomeIcon icon={faBars}/>) : (<h1 onClick={() => toggleLogIn(true)}>Log In/Register</h1>)}
+        {loggedIn ? (<FontAwesomeIcon icon={faBars} onClick={() => toggleUserDrop(!userDrop)}> </FontAwesomeIcon>) : 
+        (<h1 onClick={() => toggleLogIn(true)}>Log In/Register</h1>)}
       </div>
-      {logInToggle && <LogIn toggle={toggleLogIn}/>}
+      {logInToggle && <LogIn toggleLogin={toggleLogIn}/>}
+      {userDrop && <UserDrop toggleDrop={toggleUserDrop}/>}
     </header>
   );
 };
--- src/components/Modals/UserDrop.js
+++ src/components/Modals/UserDrop.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const UserDrop = () => {
-    return (<h1>In Progress</h1>);
+const UserDrop = props => {
+    return (
+        <div className="userDrop">
+            <div className="userDropBody">
+                <a href="#" onClick={() => props.toggleDrop(false)}>Reading History</a>
+                <a href="#" onClick={() => props.toggleDrop(false)}>About Thoth Tarot</a>
+                <a href="#" onClick={() => props.toggleDrop(false)}>Sign Out</a>
+            </div>
+        </div>
+    );
 }
 
 export default UserDrop;
