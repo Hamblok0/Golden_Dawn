c209beffd4630e2ee313a0a638cf30f284d0db4a
Joseph Cooper <hambloko@gmail.com>
2019-11-13 13:30:33 -0600

    read history placeholder html in place, just need styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Home from "./components/Home";
 import Spread from "./components/Spread";
 import DictMenu from "./components/DictMenu";
 import Dictionary from "./components/Dictionary";
+import ReadHistory from "./components/ReadHistory";
 
 const App = () => {
   return (
     <div className="main">
       <Header />
-      <Home />
+      <ReadHistory />
     </div>
   );
 };
--- src/components/ReadHistory.js
+++ src/components/ReadHistory.js
@@ -4,12 +4,57 @@ const ReadHistory = () => {
     const endpoint = "https://ds7jrtsekfc2s.cloudfront.net/t-1.png" 
     return (
         <div className="historyWrapper">
-            <div className="historyTitle">
-                <h1>Saved Readings</h1>
-            </div>
-            <div className="historyLinks">
-                <div>
-                    
+            <h1>Saved Readings</h1>
+            <div className="histories">
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
+                </div>
+                <div className="history">
+                    <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
+                    <p>11-11-19</p>
+                    <p>3:00PM</p>
                 </div>
             </div>
         </div>
