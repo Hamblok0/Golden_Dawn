44f106723c320c63e72ac09464c6c9280d46fbef
Hambloko <hambloko@gmail.com>
2019-11-15 13:53:18 -0600

    shuffle modals init

---
+++ src/components/Modals/ShuffleDrop.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ShuffleDrop = () => {
+    return (
+        foobar
+    )
+};
+
+export default ShuffleDrop;
---
+++ src/components/Modals/ShuffleMain.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ShuffleMain = () => {
+    return (
+        foobar
+    )
+}
+
+export default ShuffleMain;
