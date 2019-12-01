3475028ca4ace732b86035e948f94cb671ef2b10
Joseph Cooper <hambloko@gmail.com>
2019-11-30 23:49:53 -0600

    - Added basic React Router functionality to navigate between components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Spread from "./components/Spread";
@@ -9,8 +14,15 @@ import ReadHistory from "./components/ReadHistory";
 const App = () => {
   return (
     <div className="main">
-      <Header />
-      <Spread />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/goldendawn" component={Spread} />
+          <Route path="/dictionary" component={DictMenu} />
+          <Route path="/archive" component={ReadHistory} />
+        </Switch>
+      </Router>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import { Link } from "react-router-dom";
 import LogIn from "./Modals/LogIn";
 import UserDrop from "./Modals/UserDrop";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,11 +12,17 @@ const Header = () => {
   return (
     <header>
       <div className="title">
-        <h1>Tarot.io</h1>
+        <Link to='/'>
+          <h1>Tarot.io</h1>
+        </Link>
       </div>
       <div className="headerNav">
-        <h1>Get A Reading</h1>
-        <h1>Card Dictionary</h1>
+        <Link to='goldendawn'>
+          <h1>Get A Reading</h1>
+        </Link>
+        <Link to='dictionary'>
+          <h1>Card Dictionary</h1>
+        </Link>
         {loggedIn ? (<FontAwesomeIcon icon={faBars} onClick={() => toggleUserDrop(!userDrop)}> </FontAwesomeIcon>) : 
         (<h1 onClick={() => toggleLogIn(true)}>Log In/Register</h1>)}
       </div>
