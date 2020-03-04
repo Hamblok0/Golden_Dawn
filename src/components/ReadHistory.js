e2c49ad599314ec0807510eb9552a444db46d9d1
Hambloko <hambloko@gmail.com>
2020-03-03 23:45:08 -0600

    - Read History page now maps user readings for reading history previews - Read history previews now image of first card in the reading deck

--- src/components/ReadHistory.js
+++ src/components/ReadHistory.js
@@ -1,128 +1,60 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import decode from "jwt-decode";
 
 const ReadHistory = props => {
-    const endpoint = process.env.CARDS;
-    const api = process.env.READINGS;
-
-    const [archives, updateArchive] = useState(undefined);
+    const endpoint = "https://ds7jrtsekfc2s.cloudfront.net/";
+    const api = "https://r0pamrufoj.execute-api.us-east-2.amazonaws.com/dev/archives";
+    const [archives, updateArchive] = useState({
+        loading: true,
+        readings: []
+    });
 
     useEffect(() => {
         const user = decode(props.user);
         const readings = JSON.parse(user.archived)
         if (user && readings) {
-            
+
             const params = {
                 user: user.email,
                 readings
             }
-            console.log(params)
+
             Axios.get(api, { params })
-            .then(data => {
-                console.log(`DATA: ${JSON.stringify(data)}`);
+            .then(response => {
+                const data = {
+                    loading: false,
+                    readings: response.data.Responses.Archives.map(reading => {
+                        const img = `${endpoint}` + `${JSON.parse(reading.deck)[0]}` + '.png';
+                        return {...reading, img};
+                    })
+                }
+                updateArchive(data)
             })
             .catch(err => {
                 console.log(`AXIOS ERR: ${err}`);
             })
         }
-    },[archives])
-    
+    }, [])
+
     return (
         <div className="historyWrapper">
             <h1>Saved Readings</h1>
-            <div className="histories">
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
+            {archives.readings && 
+                <div className="histories">
+                    {archives.readings.map(reading => 
+                        <div className="history" key={reading.id}>
+                            <div className="previewWrapper">
+                                <div className="historyPreview" style={{"backgroundImage": `url(${reading.img})`}}></div>
+                            </div>
+                            <div className="timestamp">
+                                <p>11-11-19</p>
+                                <p>3:00PM</p>
+                            </div>
+                        </div>
+                    )}
                 </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-                <div className="history">
-                    <div className="previewWrapper">
-                        <div className="historyPreview" style={{"backgroundImage": `url(${endpoint})`}}></div>
-                    </div>
-                    <div className="timestamp">
-                        <p>11-11-19</p>
-                        <p>3:00PM</p>
-                    </div>
-                </div>
-            </div>
+            } 
         </div>
     )
 }
