422b99b9725bc83831e639665593b3d62e4e82c6
Hambloko <hambloko@gmail.com>
2020-03-08 02:30:40 -0500

    - Added date-fns package

--- src/components/Modals/ShuffleDrop.js
+++ src/components/Modals/ShuffleDrop.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 const ShuffleDrop = props => {
     const [number, updateNumber] = useState(1);
     const handleSubmit = e => {
-      props.setDeck(props.shuffle(props.deck, number));
-      e.preventDefault();
+        props.setDeck(props.shuffle(props.deck, number));
+        e.preventDefault();
     };
     return (
         <div className="shuffleDrop">
             <div className="triangle"></div>
             <form onSubmit={e => handleSubmit(e)}>
-                <input type="number" min="1" max="100" value={number} onChange={e => updateNumber(e.target.value)}/> 
+                <input type="number" min="1" max="100" value={number} onChange={e => updateNumber(e.target.value)} />
             </form>
         </div>
     )
