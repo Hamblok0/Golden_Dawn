cff588581934942686639e891dca4f246ada5fdb
Hambloko <hambloko@gmail.com>
2020-07-05 16:45:22 -0500

    - Installed dotenv-webpack dev dependency - Configured webpack to use dotenv module

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   module: {
@@ -48,6 +49,7 @@ module.exports = {
     new MiniCssExtractPlugin({
         filename: "[name].css",
         chunkFilename: "[id].css"
-      })
+      }),
+    new Dotenv()
   ]
 };
